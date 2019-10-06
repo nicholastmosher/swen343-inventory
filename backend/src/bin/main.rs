dc25cea07f54fd681e567588d401d5746ab7fda3
Nick Mosher <nicholastmosher@gmail.com>
2019-10-06 00:49:13 -0400

    Fix production Dockerfile

--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -7,7 +7,7 @@ fn main() -> std::io::Result<()> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL should be set");
     let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS should be set");
     println!("Launching app on {}", &bind_address);
-    erp_inventory::app::launch(database_url, bind_address)?;
+    erp::app::launch(database_url, bind_address)?;
     actor_system.run()?;
     Ok(())
 }
