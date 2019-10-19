229023cc1d8b767abdddf5708fc77a00a63813d7
Leon Kuhne <lxk1170@rit.edu>
2019-10-19 15:35:39 -0400

    added http:// and allow origin header to non working fetch item call

--- frontend/src/actions/ItemActions.ts
+++ frontend/src/actions/ItemActions.ts
@@ -1,21 +1,23 @@
-import {ThunkAction} from "redux-thunk";
-import {AnyAction} from "redux";
-import {Item, ItemActionTypes, RECEIVE_ITEMS} from "../types/ItemActionTypes";
+import { ThunkAction } from "redux-thunk";
+import { AnyAction } from "redux";
+import { Item, ItemActionTypes, RECEIVE_ITEMS } from "../types/ItemActionTypes";
 import axios from "axios";
 
-const host = "ec2-3-16-181-169.us-east-2.compute.amazonaws.com:8000";
+const host = "http://ec2-3-16-181-169.us-east-2.compute.amazonaws.com:8000";
 
 export const fetchItems =
   (): ThunkAction<void, {}, {}, AnyAction> => async dispatch => {
 
-  const value = axios.get(`${host}/api/v1/items`, {
-    headers: { "Content-Type": "application/json" }
-  })
-  .then(res => res.data)
-  .catch(e => console.log(e));
+    axios.get(`${host}/api/v1/items`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": host, // axios might need this
+      }
+    })
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
 
-  console.log(value);
-};
+  };
 
 export const receiveItems = (items: Item[]): ItemActionTypes => ({
   type: RECEIVE_ITEMS,
