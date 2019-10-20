21d8216bbc481f20e559c104456eb48e1ed2ac6e
Nick Mosher <nicholastmosher@gmail.com>
2019-10-20 16:22:56 -0400

    Change Items to a class component

--- frontend/src/view/Items.tsx
+++ frontend/src/view/Items.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { AppState } from "../reducers";
 import { Item } from "../types/Interfaces";
@@ -17,43 +17,67 @@ interface DispatchProps {
 
 type Props = StateProps & DispatchProps;
 
-const Items: React.FC<Props> = ({ items, fetchItems }) => {
-  /* <button onClick={() => fetchItems()}>Get Items</button> */
+class Items extends Component<Props, {}> {
 
-  // get this from the url (or state, or router)
-  let itemType = "warehouse"
-
-  // temporary define for items
-  let fakeItems: any[] = [{
-    address: "i live here",
-  }]
-  items = fakeItems;
-
-  // create (and fill) item cards
-  const itemDivs = []
-  for (let itemId in items) {
-    let item = items[itemId] // TODO: swap this for real id
-    const name = (<h2 className="item-card-header">{itemType} #{itemId}</h2>)
-    const itemDiv = util.itemDetails(itemType, item)
-    itemDivs.push(<div className="col-sm-6 item-card">{name}{itemDiv}</div>)
+  componentDidMount(): void {
+    this.props.fetchItems();
   }
 
+  render() {
+    const { items, fetchItems } = this.props;
 
-  return (
-    <div className="container pl-xs-0 pr-xs-0 pl-md-3 pr-md-3 content">
-      <div className="nav">
-        <div className="nav-header">
-          <h1 className="inventory-header">Inventory Management</h1>
-          <Link className="header-link" to="/catalog">Catalog View</Link>
+    // get this from the url (or state, or router)
+    let itemType = "warehouse"
+    let details
+
+    switch (itemType) {
+      case "warehouse":
+        details = (
+          <div className="warehouse-panel">
+            <h3>KennUwares prised warehouses</h3>
+          </div>
+        )
+      case "pallet":
+        // get the address from the current warehouse
+        // -> get request for warehouse from the backend
+        let address = "19, bocker street, rochester ny"
+
+        details = (
+          <div>{address}</div>
+        )
+        break
+      // TODO: add pallets, boxes and items display formats here
+      default:
+        details = (<div>Unknown Container Type</div>)
+    }
+
+    const itemDivs = []
+    for (let itemId in items) {
+      //let item = items[itemId] // TODO: swap this for real id
+
+      const name = (<h2>{itemType} #{itemId}</h2>)
+      const detailsDiv = (<div className="details">details</div>)
+      itemDivs.push(<div className="item-card col-sm-6">{name}{detailsDiv}</div>)
+    }
+
+    /* <button onClick={() => fetchItems()}>Get Items</button> */
+
+    return (
+      <div className="content">
+        <div className="nav">
+          <div className="nav-header">
+            <h1 className="inventory-header">Inventory Management</h1>
+            <Link className="header-link" to="/catalog">Catalog View</Link>
+          </div>
         </div>
+
+        <hr />
+
+        <div className="row">{itemDivs}</div>
       </div>
-
-      <hr />
-
-      <div className="row">{itemDivs}</div>
-    </div>
-  )
-};
+    )
+  }
+}
 
 const mapStateToProps = (state: AppState): StateProps => ({
   items: state.ItemReducer.items,
