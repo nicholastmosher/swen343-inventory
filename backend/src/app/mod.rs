b40cda66d082137621223addad959ec18964ac88
Nick Mosher <nicholastmosher@gmail.com>
2019-10-20 15:04:48 -0400

    Add CORS rules to let frontend reach backend

--- backend/src/app/mod.rs
+++ backend/src/app/mod.rs
@@ -6,15 +6,33 @@
 //! times horizontally without needing to coordinate any state between different
 //! instances of processes.
 
-use std::net::ToSocketAddrs;
 use actix::{Addr, SyncArbiter};
 use actix_web::{web, HttpServer, App};
 use crate::db::DbExecutor;
+use actix_cors::Cors;
 
 pub mod items;
 pub mod boxes;
 pub mod pallets;
 pub mod warehouses;
+
+pub struct AppConfig {
+    pub database_url: String,
+    pub bind_address: String,
+    pub frontend_address: Option<String>,
+}
+
+impl AppConfig {
+    pub fn from_env() -> Result<Self, String> {
+        use std::env;
+        let database_url = env::var("DATABASE_URL")
+            .map_err(|_| "failed to read DATABASE_URL environment variable")?;
+        let bind_address = env::var("BIND_ADDRESS")
+            .map_err(|_| "failed to read BIND_ADDRESS environment variable")?;
+        let frontend_address = env::var("FRONTEND_ADDRESS").ok();
+        Ok(AppConfig { database_url, bind_address, frontend_address })
+    }
+}
 
 /// The only application state used is a reference to the database Actor inbox.
 pub struct AppState {
@@ -29,20 +47,37 @@ pub struct AppState {
 /// because the runtime has the freedom to launch one or many instances of the
 /// server on multiple threads or even on multiple processes.
 ///
+/// The web application is configured using the `AppConfig` struct. You can use
+/// the `from_env` function to create an instance of `AppConfig` using values
+/// loaded from environment variables.
+///
 /// # Example
 ///
 /// ```no_run
-/// use erp::app;
+/// use erp::app::{launch, AppConfig};
 ///
-/// app::launch(
-///     "postgres://postgres:password@localhost/dbname",
-///     "127.0.0.1:8000",
-/// ).expect("should launch web server");
+/// let config = AppConfig::from_env().expect("should load AppConfig from env");
+/// launch(&config).expect("should launch web server");
 /// ```
-pub fn launch<S, A>(database_url: S, bind_address: A) -> std::io::Result<()>
-    where S: Into<String>, A: ToSocketAddrs,
+///
+/// You can also use the struct initialization syntax in order to manually
+/// override any values that you want.
+///
+/// # Example
+///
+/// ```no_run
+/// use erp::app::{launch, AppConfig};
+///
+/// let env_config = AppConfig::from_env().expect("should load AppConfig from env");
+/// launch(&AppConfig {
+///     database_url: "postgres://postgres:test-db-password@localhost/test-db".to_string(),
+///     ..env_config
+/// }).expect("should launch web server");
+/// ```
+pub fn launch(config: &AppConfig) -> std::io::Result<()>
 {
-    let database_url = database_url.into();
+    let database_url = config.database_url.clone();
+    let frontend_address = config.frontend_address.clone();
     let mut listenfd = listenfd::ListenFd::from_env();
 
     let database_addr = SyncArbiter::start(
@@ -50,16 +85,31 @@ pub fn launch<S, A>(database_url: S, bind_address: A) -> std::io::Result<()>
         move || DbExecutor::new(database_url.clone()),
     );
 
-    let mut server = HttpServer::new(move ||
+    let mut server = HttpServer::new(move || {
+        let cors = match &frontend_address {
+            Some(origin) => {
+                Cors::new()
+                    .allowed_origin(origin)
+                    .max_age(3600)
+            },
+            None => {
+                Cors::new()
+                    .allowed_methods(vec!["POST", "GET", "PUT", "DELETE"])
+                    .send_wildcard()
+                    .max_age(3600)
+            },
+        };
+
         App::new()
             .data(AppState { db: database_addr.clone() })
+            .wrap(cors)
             .configure(routes)
-    );
+    });
 
     server = if let Some(listener) = listenfd.take_tcp_listener(0)? {
         server.listen(listener)?
     } else {
-        server.bind(bind_address)?
+        server.bind(&config.bind_address)?
     };
 
     server.start();
--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,13 +1,12 @@
-use std::env;
 use dotenv::dotenv;
+use erp::app::AppConfig;
 
 fn main() -> std::io::Result<()> {
     dotenv().ok();
     let actor_system = actix::System::new("inventory");
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL should be set");
-    let bind_address = env::var("BIND_ADDRESS").expect("BIND_ADDRESS should be set");
-    println!("Launching app on {}", &bind_address);
-    erp::app::launch(database_url, bind_address)?;
+    let config = AppConfig::from_env().expect("should load app config from env");
+    erp::app::launch(&config)?;
+    println!("Launched app on {}", &config.bind_address);
     actor_system.run()?;
     Ok(())
 }
