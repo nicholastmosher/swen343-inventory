53c21ed458f3d151faf3f4ee0d2dc129d6dab6ae
Leon Kuhne <lxk1170@rit.edu>
2019-11-02 18:49:27 -0400

    renamed item to unit

--- backend/src/app/v2/items.rs
+++ backend/src/app/v2/items.rs
@@ -7,8 +7,8 @@ use crate::app::AppState;
 
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct CreateItem {
-    pub item_code: String,
+pub struct CreateUnit {
+    pub unit_code: String,
     pub quantity: u32
 }
 
@@ -16,24 +16,24 @@ pub struct CreateItem {
 pub struct Import {
     pub order_id: u32,
     pub used: bool,
-    products: Vec<CreateItem>,
-    parts: Vec<CreateItem>
+    products: Vec<CreateUnit>,
+    parts: Vec<CreateUnit>
 }
 
 
-/// Handle POST request for adding items, both parts and products, to a
+/// Handle POST request for adding units, both parts and products, to a
 /// warehouses inventory.
 pub fn add(
     state: web::Data<AppState>,
     web::Json(import): web::Json<Import>,
 ) -> impl Future<Item=HttpResponse, Error=()> {
-    println!("Adding item to box");
+    println!("Adding unit to box");
     
     let db = &state.db;
-    // TODO determine if you want to put the items into a box or a pallet
+    // TODO determine if you want to put the units into a box or a pallet
 
     // select a warehouse to add the box/pallet to
-    // create a box/pallet to put all of the items into with the warehouse (or
+    // create a box/pallet to put all of the units into with the warehouse (or
     // pallet) as parent
     // add the box/pallet to the database
 
