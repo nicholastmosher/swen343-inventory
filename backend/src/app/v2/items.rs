eae1b28c24fe97fdceb9a4a7767f41b28c4a2e2b
Nick Mosher <nicholastmosher@gmail.com>
2019-11-03 14:29:23 -0500

    Add request and response structure for receiveItems endpoint

--- backend/src/app/v2/items.rs
+++ backend/src/app/v2/items.rs
@@ -1,35 +1,44 @@
-
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
 use futures::Future;
 use futures::future::ok;
 use crate::app::AppState;
 
+#[derive(Debug, Deserialize)]
+pub struct ReceiveItemsRequest {
+    pub products: Option<Vec<PartInRequest>>,
+    pub parts: Option<Vec<PartInRequest>>,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Unit {
+pub struct ProductInRequest {
+    pub order_id: Option<i32>,
     pub item_code: String,
-    pub quantity: u32
+    pub quantity: u32,
+    pub refurbished: bool,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Import {
-    pub order_id: u32,
-    pub used: bool,
-    parts: Vec<Unit>
-    products: Vec<Unit>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PartInRequest {
+    pub item_code: String,
+    pub quantity: u32,
+    pub refurbished: bool,
 }
 
+#[derive(Debug, Serialize)]
+pub struct ReceiveItemResponse {
+    pub status: String,
+}
 
 /// Handle POST request for adding units, both parts and products, to a
 /// warehouses inventory.
 pub fn add(
     state: web::Data<AppState>,
-    web::Json(import): web::Json<Import>,
+    web::Json(received_items): web::Json<ReceiveItemsRequest>,
 ) -> impl Future<Item=HttpResponse, Error=()> {
+    let db = &state.db;
     println!("Adding unit to box");
     
-    let db = &state.db;
     // TODO determine if you want to put the units into a box or a pallet
 
     // select a warehouse to add the box/pallet to
--- backend/src/db/v2/mod.rs
+++ backend/src/db/v2/mod.rs
@@ -1,7 +1,3 @@
-//! Query definitions for the v1 API
+//! Query definitions for the v2 API
 
 pub mod items;
-pub mod boxes;
-pub mod pallets;
-pub mod warehouses;
-pub mod rules;
